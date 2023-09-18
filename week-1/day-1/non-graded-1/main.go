460e6e71336ebfef9cc40133ed3aaf37b4a2b009
Tango992 <daniel.rahmanto@gmail.com>
2023-09-18 16:01:00 +0700

    non graded challenge 1

--- week-1/day-1/non-graded-1/main.go
+++ week-1/day-1/non-graded-1/main.go
@@ -5,49 +5,72 @@ import (
 	"strconv"
 )
 
+
 func main() {
+	variable1()
+	variable2()
+	cli()
+	array1()
+	array2()
+}
+
+
+func variable1() {
 	var myNum int32 = 50
 	fmt.Printf("myNum: %v\n", myNum)
 	var myNum2 float32 = 51
 	fmt.Printf("myNum2: %v\n", myNum2)
 	myNumStr := "50"
 	fmt.Printf("myNumStr: %v\n\n", myNumStr)
+}
 
+
+func variable2() {
 	var x, y int32 = 5, 10
 	z := x + y
 	fmt.Printf("z: %v\n\n", z)
+}
 
+
+func cli() {
 	var name string
 	fmt.Print("Masukkan nama: ")
 	fmt.Scanln(&name)
 	fmt.Printf("Hello, %v!\n\n", name)
+}
 
+
+func array1() {
 	people := []string{"Walt", "Jesse", "Skyler", "Saul"}
 	fmt.Println(people)
 	fmt.Printf("Panjang slice people: %v\n", len(people))
 	people = append(people, "Hank", "Marie")
 	fmt.Println(people)
 	fmt.Printf("Panjang slice people: %v\n\n", len(people))
+}
 
-	people2 := make([][]string, 3)
 
-	for i := range people2 {
+func array2() {
+	arr := [3]map[string]string{}
+	
+	for i := 0; i < 3; i++ {
 		var name, gender string
 		fmt.Print("Name\t: ")
 		fmt.Scanln(&name)
 		fmt.Print("Gender\t: ")
 		fmt.Scanln(&gender)
 		fmt.Println()
-		people2[i] = []string{strconv.Itoa(i), name, gender}
-	}
-	fmt.Printf("Before\t: %v\n", people2)
 
-	for i := range people2 {
-		if people2[i][2] == "M" {
-			people2[i][1] = "Mr. " + people2[i][1]
-		} else if people2[i][2] == "F" {
-			people2[i][1] = "Mrs. " + people2[i][1]
+		arr[i] = map[string]string{"key": strconv.Itoa(i), "name": name, "gender": gender}
+	}
+	fmt.Printf("Before\t: %v\n", arr)
+
+	for i := 0; i < 3; i++ {
+		if arr[i]["gender"] == "M" {
+			arr[i]["name"] = "Mr. " + arr[i]["name"]
+		} else if arr[i]["gender"] == "F" {
+			arr[i]["name"] = "Mr. " + arr[i]["name"]
 		}
 	}
-	fmt.Printf("After\t: %v\n\n", people2)
+	fmt.Printf("After\t: %v\n", arr)
 }
--- week-1/day-1/pm-challenge/main.go
+++ week-1/day-1/pm-challenge/main.go
@@ -27,5 +27,4 @@ func main() {
 	
 	fmt.Printf("%.6f\n", k)
 	fmt.Printf("%E\n", k)
-
 }
