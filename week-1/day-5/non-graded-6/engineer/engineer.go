b95c60c706632266818f1a9fa4d5d0f01a14c83e
Tango992 <daniel.rahmanto@gmail.com>
2023-09-21 23:33:37 +0700

    finished code on interface challenge

--- week-1/day-5/non-graded-6/engineer/engineer.go
+++ week-1/day-5/non-graded-6/engineer/engineer.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"math/rand"
+	"fmt"
 )
 
 type Employee interface {
@@ -11,21 +11,41 @@ type Employee interface {
 
 type Engineer struct {
     Name string
+    Aged int
 }
 
 func (e Engineer) Age() int {
-	return rand.Intn(25)
+	return e.Aged
 }
 
 func (e Engineer) Language() string {
     return e.Name + " programs in Go"
 }
 
+type Developer struct {
+    Name string
+    Aged int
+}
+
+func (e Developer) Age() int {
+	return e.Aged
+}
+
+func (e Developer) Language() string {
+    return e.Name + " programs in JS"
+}
+
+
 func main() {
     // This will throw an error
     var programmers []Employee
-    elliot := Engineer{Name: "Elliot"}
+    elliot := Engineer{Name: "Elliot", Aged: 40}
+    john := Developer{Name: "John", Aged: 30}
     // Engineer does not implement the Employee interface
     // you'll need to implement Age() and Random()
-    programmers = append(programmers, elliot)
+    programmers = append(programmers, elliot, john)
+
+    for _, programmer := range programmers {
+        fmt.Printf("%v, aged %v years old.\n", programmer.Language(), programmer.Age())
+    }
 }
