363a1fab23738f529c95649f9fc24fb2add6b6d7
Tango992 <daniel.rahmanto@gmail.com>
2023-09-25 10:04:16 +0700

    finished calculator in go

--- week-2/day-1/preview/calculator/calculator.go
+++ week-2/day-1/preview/calculator/calculator.go
@@ -14,22 +14,28 @@ func main() {
 	calculator(os.Args[1], os.Args[2], os.Args[3])
 }
 
-func calculator(operation string, v1 string, v2 string) {
-	i, err1 := strconv.Atoi(v1)
-	j, err2 := strconv.Atoi(v2)
+func calculator(operation string, value1 string, value2 string) {
+	input1, err1 := strconv.Atoi(value1)
+	input2, err2 := strconv.Atoi(value2)
 	if err1 != nil || err2 != nil {
 		fmt.Println("Input is not an integer!")
 		os.Exit(1)
 	}
+	i := float64(input1)
+	j := float64(input2)
 
 	switch operation {
 	case "add":
+		fmt.Print("Result: ")
 		add(i, j)
 	case "sub":
+		fmt.Print("Result: ")
 		sub(i, j)
 	case "mul":
+		fmt.Print("Result: ")
 		mul(i, j)
 	case "div":
+		fmt.Print("Result: ")
 		div(i, j)
 	default:
 		fmt.Println("Invalid operation!")
@@ -37,18 +43,18 @@ func calculator(operation string, v1 string, v2 string) {
 	}
 }
 
-func add(i int, j int) {
-
+func add(i float64, j float64) {
+	fmt.Printf("%.2f\n", i + j)
 }
 
-func sub(i int, j int) {
-
+func sub(i float64, j float64) {
+	fmt.Printf("%.2f\n", i - j)
 }
 
-func mul(i int, j int) {
-
+func mul(i float64, j float64) {
+	fmt.Printf("%.2f\n", i * j)
 }
 
-func div(i int, j int) {
-
+func div(i float64, j float64) {
+	fmt.Printf("%.2f\n", i / j)
 }
