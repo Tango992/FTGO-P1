82d8cffca10d0709db98627bbb803adc09c93d0f
Tango992 <daniel.rahmanto@gmail.com>
2023-09-26 09:43:02 +0700

    goroutines with channels

---
+++ week-2/day-2/excercise/channel-image-processing/go.mod
@@ -0,0 +1,3 @@
+module image-procesing-channel
+
+go 1.21.1
---
+++ week-2/day-2/excercise/channel-image-processing/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func processImage(i int, ImageURL string, channel chan string) {
+	fmt.Printf("Processing image: %v\n", ImageURL)
+	time.Sleep(3 * time.Second)
+	fmt.Printf("Image processing completed: %v\n", ImageURL)
+	channel <- fmt.Sprintf("Iteration = %v", i+1)
+}
+
+func main() {
+	now := time.Now()
+	channel := make(chan string)
+	images := []string{
+		"https://example.com/image1.jpg",
+		"https://example.com/image2.jpg",
+		"https://example.com/image3.jpg",
+		"https://example.com/image4.jpg",
+	}
+
+	for i, image := range images {
+		go processImage(i, image, channel)
+	}
+	fmt.Println(<- channel)
+	fmt.Println(<- channel)
+	fmt.Println(<- channel)
+	fmt.Println(<- channel)
+	fmt.Printf("Elapsed time: %v\n", time.Since(now))
+}
