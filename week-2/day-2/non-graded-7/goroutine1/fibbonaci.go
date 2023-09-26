955c95670c43b888e50383daaf97c6cfa97b1c5b
Tango992 <daniel.rahmanto@gmail.com>
2023-09-26 11:17:26 +0700

    fibbonaci with gourutine

---
+++ week-2/day-2/non-graded-7/goroutine1/fibbonaci.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func main() {
+	// with goroutine: 500-600 micro seconds
+	// without goroutine: 190-250 micro seconds
+	now := time.Now()
+	var wg sync.WaitGroup
+	for i := 1; i <= 200; i++ {
+		wg.Add(1)
+		go func(i int){
+			fib(i)
+			wg.Done()
+		}(i)
+	}
+	wg.Wait()
+	fmt.Println(time.Since(now))
+}
+
+func fib(n int){
+	var a uint64 = 0
+	var b uint64 = 1
+	var c uint64
+	if n == 0 {
+		fmt.Println(a)
+		return
+	} else if n == 1{
+		fmt.Println(b)
+		return
+	} else {
+		for i := 2; i <= n; i++ {
+			c = a + b
+			a = b
+			b = c
+		}
+		fmt.Println(b)
+		return
+	}
+	
+}
