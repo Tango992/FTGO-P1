44f457bc18a9c4c5c02afe85f2ff538b53ed4952
Tango992 <daniel.rahmanto@gmail.com>
2023-10-01 22:31:54 +0700

    preview week 1

---
+++ week-3/day-1/preview/word-count/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+	"sync"
+)
+
+func main() {
+	var wg sync.WaitGroup
+	var mutex sync.Mutex
+
+	file, err := os.Open("sample.txt")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer file.Close()
+
+	result := scanLine(file, &wg, &mutex)
+	wg.Wait()
+	displayResult(result)
+}
+
+func scanLine(file *os.File, wg *sync.WaitGroup, mutex *sync.Mutex) map[string]int {
+	scanner := bufio.NewScanner(file)
+	result := map[string]int{}
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		sliceOfWords := strings.Fields(line)
+		for _, word := range sliceOfWords {
+			wg.Add(1)
+			go func(s string){
+				defer wg.Done()
+				mutex.Lock()
+				result[s]++
+				mutex.Unlock()
+			}(word)
+		}
+	}
+	return result
+}
+
+func displayResult(result map[string]int) {
+	fmt.Println("Word count:")
+	for key, value := range result {
+		fmt.Printf("%v\t: %v\n", key, value)
+	}
+}
